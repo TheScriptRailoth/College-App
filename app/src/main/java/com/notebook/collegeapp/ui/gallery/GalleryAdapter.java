77888a377372f9c89d38c137849361a49a5eb900
Aviral Jain <aviraljainltr@gmail.com>
2023-05-14 16:41:20 +0530

    COLLEGE APP: Added code to fetch images from database

---
+++ app/src/main/java/com/notebook/collegeapp/ui/gallery/GalleryAdapter.java
@@ -0,0 +1,57 @@
+package com.notebook.collegeapp.ui.gallery;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.notebook.collegeapp.R;
+
+import java.util.List;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewAdapter> {
+
+    private Context context;
+    private List<String> images;
+
+    public GalleryAdapter(Context context, List<String> images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public GalleryViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.gallery_image,parent ,false);
+        return new GalleryViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GalleryViewAdapter holder, int position) {
+
+        Glide.with(context).load(images.get(position)).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class GalleryViewAdapter extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        public GalleryViewAdapter(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.image);
+
+        }
+    }
+
+}
--- app/src/main/java/com/notebook/collegeapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/notebook/collegeapp/ui/gallery/GalleryFragment.java
@@ -2,22 +2,127 @@ package com.notebook.collegeapp.ui.gallery;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.notebook.collegeapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GalleryFragment extends Fragment {
+
+    RecyclerView dequinox,cultural,others;
+    GalleryAdapter adapter;
+    DatabaseReference reference;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view= inflater.inflate(R.layout.fragment_gallery, container, false);
+        dequinox = view.findViewById(R.id.dequinox);
+        cultural = view.findViewById(R.id.cultural);
+        others = view.findViewById(R.id.others);
+        
+        reference = FirebaseDatabase.getInstance().getReference().child("gallery");
+        
+        getDequinoxImage();
+        getCulturalImage();
+        getOthersImage();
+        return view;
+
+    }
+
+    private void getOthersImage() {
+        reference.child("Others").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+
+                    String data = (String) snapshot.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                others.setLayoutManager(new GridLayoutManager(getContext(),3));
+                others.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+
+            }
+        });
+    }
+
+    private void getCulturalImage() {
+        reference.child("Cultural").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+
+                    String data = (String) snapshot.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                cultural.setLayoutManager(new GridLayoutManager(getContext(),3));
+                cultural.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+
+            }
+        });
+    }
+
+    private void getDequinoxImage() {
+        reference.child("Dequinox").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+
+                    String data = (String) snapshot.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                dequinox.setLayoutManager(new GridLayoutManager(getContext(),3));
+                dequinox.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+
+            }
+        });
     }
 }
