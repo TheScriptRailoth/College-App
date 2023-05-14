efa3c80b7df9821a6a7897f01423f60daa3f1f23
TheScriptRailoth <am3718440@gmail.com>
2023-05-14 06:22:12 +0530

    College App: Added Automatic Image Slider
    Signed-off-by: TheScriptRailoth <am3718440@gmail.com>

--- app/src/main/java/com/notebook/collegeapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/notebook/collegeapp/ui/home/HomeFragment.java
@@ -7,17 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.notebook.collegeapp.R;
 
 
 public class HomeFragment extends Fragment {
+    private ViewFlipper flipper;
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+        int imarray[]={R.drawable.pic1,R.drawable.pic2, R.drawable.pic3, R.drawable.pic4,R.drawable.pic5, R.drawable.pic6, R.drawable.pic7};
+        flipper=view.findViewById(R.id.flipper);
+        for(int i=0; i< imarray.length;i++)
+            showImage(imarray[i]);
+        return view;
+    }
+    public void showImage(int img)
+    {
+        ImageView imageview=new ImageView(getActivity());
+        imageview.setBackgroundResource(img);
+        flipper.addView(imageview);
+        flipper.setAutoStart(true);
+        flipper.setFlipInterval(2700);
+        flipper.setInAnimation(getActivity(),android.R.anim.slide_in_left);
+        flipper.setOutAnimation(getActivity(),android.R.anim.slide_out_right);
     }
 }
