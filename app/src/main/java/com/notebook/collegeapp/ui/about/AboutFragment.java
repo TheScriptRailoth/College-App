a7dba3a989b70059fda5657d2a1036249be823a8
Aviral Jain <aviraljainltr@gmail.com>
2023-05-14 17:50:11 +0530

    COLLEGE APP: Added viewpager in about section

--- app/src/main/java/com/notebook/collegeapp/ui/about/AboutFragment.java
+++ app/src/main/java/com/notebook/collegeapp/ui/about/AboutFragment.java
@@ -3,21 +3,52 @@ package com.notebook.collegeapp.ui.about;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.notebook.collegeapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AboutFragment extends Fragment {
+
+    private ViewPager viewPager;
+    private BranchAdapter adapter;
+    private List<BranchModel> list;
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        list = new ArrayList<>();
+        list.add(new BranchModel(R.drawable.ic_computer,"Computer science", "sadyfyue efy8eyf 8fysuidyfuiueyi dfhuhf djfheu ejdjhe"));
+        list.add(new BranchModel(R.drawable.ic_mechanical,"Mechanical", "sadyfyue efy8eyf 8fysuidyfuiueyi dfhuhf djfheu ejdjhe"));
+        list.add(new BranchModel(R.drawable.ic_ec,"Electronics", "sadyfyue efy8eyf 8fysuidyfuiueyi dfhuhf djfheu ejdjhe"));
+        list.add(new BranchModel(R.drawable.ic_civil,"Civil", "sadyfyue efy8eyf 8fysuidyfuiueyi dfhuhf djfheu ejdjhe"));
+        list.add(new BranchModel(R.drawable.ic_chemical,"Chemical", "sadyfyue efy8eyf 8fysuidyfuiueyi dfhuhf djfheu ejdjhe"));
+
+        adapter= new BranchAdapter(getContext(),list);
+
+        viewPager = view.findViewById(R.id.viewPager);
+
+        viewPager.setAdapter(adapter);
+
+        ImageView imageView = view.findViewById(R.id.collegeImage);
+
+        Glide.with(getContext()).load("https://www.juet.ac.in/images/show/1.jpg").into(imageView);
+
+        return view;
+
     }
 }
---
+++ app/src/main/java/com/notebook/collegeapp/ui/about/BranchAdapter.java
@@ -0,0 +1,62 @@
+package com.notebook.collegeapp.ui.about;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.notebook.collegeapp.R;
+
+import java.util.List;
+
+public class BranchAdapter extends PagerAdapter {
+    private Context context;
+    private List<BranchModel> list;
+
+    public BranchAdapter(Context context, List<BranchModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.branch_item_layout,container,false);
+
+        ImageView branchIcon;
+        TextView branchTitle,branchDescription;
+
+        branchIcon = view.findViewById(R.id.branchIcon);
+        branchTitle= view.findViewById(R.id.branchTitle);
+        branchDescription= view.findViewById(R.id.branchDescription);
+
+        branchIcon.setImageResource(list.get(position).getImg());
+        branchTitle.setText(list.get(position).getTitle());
+        branchDescription.setText(list.get(position).getDescription());
+
+        container.addView(view,0);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
