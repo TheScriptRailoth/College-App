1062fc497dbf22abc33c60d02030eab381dbeeed
TheScriptRailoth <am3718440@gmail.com>
2023-05-16 15:44:14 +0530

    COLLEGE APP: Added share option to share app link
    Signed-off-by: TheScriptRailoth <am3718440@gmail.com>

--- app/src/main/java/com/notebook/collegeapp/MainActivity.java
+++ app/src/main/java/com/notebook/collegeapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -13,8 +14,11 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -23,6 +27,9 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
 import com.notebook.collegeapp.ebook.EbookActivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -95,6 +102,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         else if (item.getItemId() == R.id.navigation_share)
         {
+            String url="https://github.com/TheScriptRailoth/College-App";
+            Intent intent=new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plane");
+            intent.putExtra(Intent.EXTRA_SUBJECT,"Checkout this application");
+            intent.putExtra(Intent.EXTRA_TEXT,"Your Application Link Is Here: \n"+url);
+            startActivity(Intent.createChooser(intent,"Share Via"));
             Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
         }
         else if (item.getItemId() == R.id.navigation_theme)
@@ -111,7 +124,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return true;
     }
-
     @Override
     public void onBackPressed() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START)){
